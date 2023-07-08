0ba9415c9455564e85e0cf42735d1a2fa056fa77
ktmurphy <katie.m.murphy@outlook.com>
2023-07-08 09:34:15 -0600

    worked on app 8

---
+++ udemyPyProFilesMaster/App-8-Instant-Dictionary-Webapp/definition.py
@@ -0,0 +1,15 @@
+
+import pandas
+
+class Definition:
+
+    def __init__(self, term):
+        
+        self.term = term
+
+    def get(self):
+        df = pandas.read_csv('data.csv')
+        # the df returns a word dataframe, which we get the term out of, and then convert to tuple
+        return tuple(df.loc[df['word'] == self.term]['definition'])
+definition = Definition('biotope').get()
+print(definition)
---
+++ udemyPyProFilesMaster/App-8-Instant-Dictionary-Webapp/webapp/home.py
@@ -0,0 +1,18 @@
+import justpy as jp
+
+
+class Home:
+
+    path = "/"
+
+    def serve(self):
+        wp = jp.QuasarPage(tailwind=True)
+        div = jp.Div(a=wp, classes="bg-grey h-screen")
+        jp.Div(a=div, text="This is the Home page!", classes= "text-4xl m-2")
+        jp.Div(a=div, text="""
+        A web app that lets users type in a term in a text box and returns the English definition
+of that term instantly as soon as the user has finished typing.
+The web app consists of a website with a navigation menu, a Home, Dictionary, and About page.
+""", classes="text-lg")
+        return wp
+
